a6476a15905adb2a19e5af50d04e957b9297b3b0
dmitrii-pov <popkovd.o@yandex.ru>
2026-03-17 21:06:49 +0300

    fix: remove trailing comma in RouteNotFoundException for KPHP compatibility

--- src/Exception/RouteNotFoundException.php
+++ src/Exception/RouteNotFoundException.php
@@ -12,7 +12,7 @@ final class RouteNotFoundException extends \RuntimeException
     public function __construct(string $method, string $path)
     {
         parent::__construct(
-            sprintf('No route found for %s %s', strtoupper($method), $path),
+            sprintf('No route found for %s %s', strtoupper($method), $path)
         );
     }
 }
